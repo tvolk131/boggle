f444cdc4e2b2ce62276a73ba100d3ff81c103677
Tommy Volk <tvolk131@gmail.com>
2017-11-01 22:48:38 -0500

    Displays logo at the top

---
+++ client/src/components/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+const Title = (props) => (
+  <div>
+    <img src={logo} />
+  </div>
+);
+
+export default Title;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import Title from './components/Title.jsx';
 import Board from './components/Board.jsx';
 import Scoreboard from './components/Scoreboard.jsx';
 import './styles.css'
@@ -10,6 +11,7 @@ import './styles.css'
 render(
   <Provider store={store}>
     <div className='game-wrapper'>
+      <Title />
       <Board />
       <Scoreboard />
     </div>
