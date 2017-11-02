dc6ab879bdbdc9f32b05cde9dc4b045b14f4ee73
Tommy Volk <tvolk131@gmail.com>
2017-11-01 22:22:26 -0500

    Redux has action for submitting the current word

--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -1,6 +1,20 @@
+const SUBMIT_WORD = 'SUBMIT_WORD';
+
 export default (state, {type, payload}) => {
   switch (type) {
+    case SUBMIT_WORD:
+      return {
+        ...state,
+        completedWords: [...state.completedWords, currentWord],
+        currentWord: '',
+        currentWordTiles: {}
+      }
     default:
       return state
   }
 };
+
+export const submitWord = payload => ({
+  type: SUBMIT_WORD,
+  payload
+});
